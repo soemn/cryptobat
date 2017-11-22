2ac06f86f276712818d07db7acc463721727fd6e
sm <a0086845@u.nus.edu>
2017-11-22 23:57:25 +0800

    updated html to add materialize css

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <p>Ready</p>
       </div>
     )
