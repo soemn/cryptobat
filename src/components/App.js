3ade56f980207880ec6d023925f4d1d166cfeeea
hello <31865878+mingyikoh@users.noreply.github.com>
2017-11-29 17:16:50 +0800

    add landing page frame

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="container">
-        <p>Ready</p>
+      <div className='container'>
+
+        <div className='fixedStuff'>
+          <div className='navigation-bar'>
+            <p>CRYPTOBAT</p>
+          </div>
+
+          <div className='left-account-summary'>
+            <p>Account Summary</p>
+            {/* render API res for GET https://bittrex.com/api/v1.1/account/getbalances?apikey=API_KEY */}
+          </div>
+          <div className='top-chart'>
+            <p>Chart</p>
+            {/* render API res from chart */}
+          </div>
+          <div className='middle-orders'>
+            <p>Open Orders/Place Orders</p>
+            {/* render API res for GET https://bittrex.com/api/v1.1/account/getbalances?apikey=API_KEY
+ */}
+          </div>
+          <div className='top-right-news'>
+            <p>News</p>
+          </div>
+          <div className='bottom-right-alt-data'>
+            <p>Alt Data</p>
+          </div>
+        </div>
+
+        <div className='bottom-trading-strategy'>
+          <p>Trading Strategy #1</p>
+        </div>
       </div>
     )
   }
