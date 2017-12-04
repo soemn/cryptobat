1faa350c3087a1f66fdaba57981dd01a825c8082
Persis Ngan <persisngan@gmail.com>
2017-12-04 17:06:57 +0800

    mapped get response to log only titles

--- src/reducers/news.js
+++ src/reducers/news.js
@@ -3,19 +3,32 @@ import axios from 'axios'
 export const LOADNEWS = 'reducers/LOADNEWS'
 
 const initialState = {
-  title: 'test title'
+  title: 'test title',
+  token: 'btc'
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case LOADNEWS:
-      axios.get('http://localhost:9000/cryptoPanic/ETH').then(response => {
-        console.log(response)
-      })
+      axios
+        .get('http://localhost:9000/cryptoPanic/' + state.token)
+        // .then(response => {
+        //   console.log(response)
+        // })
+        .then(response => {
+          // let headline = response.data.map(header => {
+          let headline = response.data.results.map(header => {
+            console.log(header.title)
+          })
+          //   console.log(headline)
+        })
       return {
         ...state,
         title: state.title
       }
+      // })
+      // })
+      break
     default:
       return state
   }
