b981f97771c9c6d48b2ff3720555269a420cadfe
Ray Tham <RayWTham@gmail.com>
2017-12-04 12:55:42 +0800

    merge

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,27 @@
-import React from "react"
+import React from 'react'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import { bindActionCreators } from 'redux'
+import { loadNews } from '../reducers/news'
 
-const News = () => (
+const News = props => (
   <div>
     <h5>News</h5>
+    <button onClick={props.loadNews}> Load News </button>
   </div>
 )
 
-export default News
+const mapStateToProps = state => ({
+  title: state.news.title
+})
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      loadNews,
+      changePage: () => push('/News')
+    },
+    dispatch
+  )
+
+export default connect(mapStateToProps, mapDispatchToProps)(News)
