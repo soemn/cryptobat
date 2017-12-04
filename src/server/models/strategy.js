925bb0ec2c5c73e48ed2c348006aea6a3c59cd84
Ray Tham <RayWTham@gmail.com>
2017-12-04 20:36:38 +0800

    strategy schema: added conditions & executions

--- src/server/models/strategy.js
+++ src/server/models/strategy.js
@@ -1,16 +1,14 @@
 var mongoose = require("mongoose")
 var Schema = mongoose.Schema
 
-var strategySchema = new Schema(
+var executionSchema = new Schema(
   {
     TradeType: String, // tradebuy or tradesell
-    MarketName: String, // BTC-ETH, BTC-OMG, ETH-OMG
     Quantity: Number,
     Rate: Number,
-    ConditionType: String, // supported options are 'NONE', 'GREATER_THAN', 'LESS_THAN'
-    Target: Number, // used in conjunction with ConditionType
-
     // Fixed. No input allowed by users
+    ConditionType: { type: String, default: "NONE" }, // supported options are 'NONE', 'GREATER_THAN', 'LESS_THAN'
+    Target: { type: Number, default: 0 }, // used in conjunction with ConditionType
     OrderType: { type: String, default: "LIMIT" },
     TimeInEffect: { type: String, default: "GOOD_TIL_CANCELLED" }
   },
@@ -19,11 +17,20 @@ var strategySchema = new Schema(
   }
 )
 
+var conditionSchema = new Schema({
+  Type: String,
+  Value: Number
+})
+
+var strategySchema = new Schema({
+  MarketName: String, // BTC-ETH, BTC-OMG, ETH-OMG
+  conditions: [conditionSchema],
+  executions: [executionSchema]
+})
+
 strategySchema.methods.trade = function() {
   console.log(
-    `This is a schema method. The trade type for this strategy is ${
-      this.OrderType
-    }`
+    `This is a schema method. The type for this strategy is ${this.type}`
   )
 }
 
