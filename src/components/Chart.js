8a2056e40ab831d055220ebdf66adc0496fa69f0
Persis Ngan <persisngan@gmail.com>
2017-12-05 15:57:58 +0800

    removed news from chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -29,7 +29,6 @@ class Chart extends Component {
       toolbar_bg: '#f1f3f6',
       enable_publishing: false,
       allow_symbol_change: true,
-      news: ['headlines'],
       hideideas: true
     })`
     document.body.appendChild(tradingWidget)
